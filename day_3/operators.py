cefde381c7643ba8a6d4b710acb0cb3c1df0a8f1
CudraMohamed <ncudramohamed@gmail.com>
2022-05-06 09:00:41 +0300

    intro changes

--- day_3/operators.py
+++
@@ -1,8 +0,0 @@
-#day3 operators
-age = 22
-height = 10.7
-basic_pay = 1902039.383839
-base = input("Enter base :")
-height = input("Enter height:")
-area = 3 * base * height
-print(area)
--- day_7/sets.py
+++
@@ -1,3 +0,0 @@
-set = {}
-set_utensils = {"spoons","plates","sufuria","sieve","grinder","jug"}
-print(len(set_utensils))
