3456797eee8db289d983b8c2fb543395393390ba
CudraMohamed <ncudramohamed@gmail.com>
2022-05-06 19:28:23 +0300

    operation & exercises

---
+++ day_1/intro.py
@@ -0,0 +1,55 @@
+#Arithmetic/mathematical operations 
+a=2+3     #addition
+print(a)
+b=3-2     #subtraction
+print(b)
+c=3*2     #multiplication
+print(c)
+d=3/2     #division
+print(d)
+e=3^2   #3x3=9 3 raised to power 2
+print(e)
+f=3%2    #modulus-outputs the remainder
+print(f)
+g=3//2   #floor division-prints out the result wthout the value after the decimal
+print(g)
+h=3**2   #exponentiation operator -equivalent to 3X3
+print(h)
+
+         #Exercise
+  #operations
+i=3+4  #addition
+print(i)
+j=4-3  #subtraction
+print(j)
+k=3*4   #multiplication
+print(k)
+l=3%4   #modulus
+print(l)
+m=4/3   #division
+print(m)
+n=3**4   #exponention
+print(n)
+o=3//4   #floor division
+print(o)
+
+#Strings
+p="Cudra"
+print(p)
+q="Morehermade"
+print(q)
+r="Kenya"
+print(r)
+s="I am enjoying 30 days of python"
+print(s)
+
+#Checking data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Cudra','Python','Kenya']))
+print(type('Cudra'))
+print(type('Morehermade'))
+print(type('Kenya'))
+ # Find a Euclidian distance between (2,3) and (10,8)
