f11c5a997e2cc9d55c79c26e51fd3c3a7870247a
CudraMohamed <ncudramohamed@gmail.com>
2022-05-27 21:26:37 +0300

    String methods

--- 04_Day_Strings/day_4/strings.py
+++ 04_Day_Strings/day_4/strings.py
@@ -93,3 +93,118 @@ print(cda)
 stationery='pens'
 pn=stationery[0:4:3]
 print(pn)
+
+#String Methods
+challenge = 'thirty days of python'
+print(challenge.capitalize())       #Capitalizes the first letter of a string
+print(challenge.count('th'))        #returns occurences of 'sub-strings'
+print(challenge.endswith('n'))      #checks if a string ends with a specified ending
+challenges = 'thirty\tdays\tof\tpython'
+print(challenges.expandtabs())
+print(challenges.expandtabs(10))        #expands tabs size with spaces
+print(challenges.find('y'))             #returns the index of the first occurrence substring if none then returns -1
+print(challenges.rfind('y'))            #returns the index of the last occurrence substring if none then returns -1
+radius = 10
+pi=3.14
+area=pi*radius**2
+results= 'The area of a circle with radius {} is {}.'.format(str(radius),str(area))     #formats strings into a nicer output
+print(results)
+#index(): Returns the lowest index of a substring, additional arguments indicate starting and ending index (default 0 and string length - 1). If the substring is not found it raises a valueError.
+challenge = 'thirty days of python'
+sub_string = 'da'
+print(challenge.index(sub_string))  # 7
+print(challenge.index(sub_string, 9)) # error
+#
+#rindex(): Returns the highest index of a substring, additional arguments indicate starting and ending index (default 0 and string length - 1)
+challenge = 'thirty days of python'
+sub_string = 'da'
+print(challenge.rindex(sub_string))  # 8
+print(challenge.rindex(sub_string, 9)) # error
+#isalnum(): Checks alphanumeric character
+challenge = 'ThirtyDaysPython'
+print(challenge.isalnum()) # True
+
+challenge = '30DaysPython'
+print(challenge.isalnum()) # True
+
+challenge = 'thirty days of python'
+print(challenge.isalnum()) # False, space is not an alphanumeric character
+
+challenge = 'thirty days of python 2019'
+print(challenge.isalnum()) # False
+#isalpha(): Checks if all string elements are alphabet characters (a-z and A-Z)
+challenge = 'thirty days of python'
+print(challenge.isalpha()) # False, space is once again excluded
+challenge = 'ThirtyDaysPython'
+print(challenge.isalpha()) # True
+num = '123'
+print(num.isalpha())      # False
+#isdecimal(): Checks if all characters in a string are decimal (0-9)
+challenge = 'thirty days of python'
+print(challenge.isdecimal())  # False
+challenge = '123'
+print(challenge.isdecimal())  # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # False
+challenge = '12 3'
+print(challenge.isdecimal())  # False, space not allowed
+#isdigit(): Checks if all characters in a string are numbers (0-9 and some other unicode characters for numbers)
+challenge = 'Thirty'
+print(challenge.isdigit()) # False
+challenge = '30'
+print(challenge.isdigit())   # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # True
+#isnumeric(): Checks if all characters in a string are numbers or number related (just like isdigit(), just accepts more symbols, like ½)
+num = '10'
+print(num.isnumeric()) # True
+num = '\u00BD' # 1/2
+print(num.isnumeric()) # True
+num = '10.5'
+print(num.isnumeric()) # False
+#isidentifier(): Checks for a valid identifier - it checks if a string is a valid variable name
+challenge = '30DaysOfPython'
+print(challenge.isidentifier()) # False, because it starts with a number
+challenge = 'thirty_days_of_python'
+print(challenge.isidentifier()) # True
+#islower(): Checks if all alphabet characters in the string are lowercase
+challenge = 'thirty days of python'
+print(challenge.islower()) # True
+challenge = 'Thirty days of python'
+print(challenge.islower()) # False
+#isupper(): Checks if all alphabet characters in the string are uppercase
+challenge = 'thirty days of python'
+print(challenge.isupper()) #  False
+challenge = 'THIRTY DAYS OF PYTHON'
+print(challenge.isupper()) # True
+#join(): Returns a concatenated string
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = ' '.join(web_tech)
+print(result) # 'HTML CSS JavaScript React'
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = '# '.join(web_tech)
+print(result) # 'HTML# CSS# JavaScript# React'
+#strip(): Removes all given characters starting from the beginning and end of the string
+challenge = 'thirty days of pythoonnn'
+print(challenge.strip('noth')) # 'irty days of py'
+#replace(): Replaces substring with a given string
+challenge = 'thirty days of python'
+print(challenge.replace('python', 'coding')) # 'thirty days of coding'
+#split(): Splits the string, using given string or space as a separator
+challenge = 'thirty days of python'
+print(challenge.split()) # ['thirty', 'days', 'of', 'python']
+challenge = 'thirty, days, of, python'
+print(challenge.split(', ')) # ['thirty', 'days', 'of', 'python']
+#title(): Returns a title cased string
+challenge = 'thirty days of python'
+print(challenge.title()) # Thirty Days Of Python
+#swapcase(): Converts all uppercase characters to lowercase and all lowercase characters to uppercase characters
+challenge = 'thirty days of python'
+print(challenge.swapcase())   # THIRTY DAYS OF PYTHON
+challenge = 'Thirty Days Of Python'
+print(challenge.swapcase())  # tHIRTY dAYS oF pYTHON
+#startswith(): Checks if String Starts with the Specified String
+challenge = 'thirty days of python'
+print(challenge.startswith('thirty')) # True
+challenge = '30 days of python'
+print(challenge.startswith('thirty')) # False
