9dbc80b808f462a50736dd8e141da9f03dd1d954
CudraMohamed <ncudramohamed@gmail.com>
2022-05-31 22:31:39 +0300

    List exercise

--- 05_Day_Lists/day_5/lists.py
+++ 05_Day_Lists/day_5/lists.py
@@ -133,42 +133,56 @@ print(it_companies)
 it_companies.insert(3,"Tech Mahindra")
 print(it_companies)
 # Change one of the it_companies names to uppercase (IBM excluded!)
-
+companies_upper=[company.upper() for company in it_companies]
+print(companies_upper)
 # Join the it_companies with a string '#;  '
-
+joined_list='#'.join(it_companies)
+print(joined_list)
 # Check if a certain company exists in the it_companies list.
-
+in_list="iHub" in it_companies
+print(in_list)
 # Sort the list using sort() method
-
+companies=['HCL Technologies', 'Google', 'Microsoft', 'Tech Mahindra', 'Apple', 'IBM', 'Oracle', 'Amazon', 'Oracle']
+companies.sort()
+print(companies)
 # Reverse the list in descending order using reverse() method
-
-# Slice out the first 3 companies from the list
-
+companies.reverse()
+print(companies)
 # Slice out the last 3 companies from the list
-
+del companies[0:2]
+print(companies)
 # Slice out the middle IT company or companies from the list
-
+print(companies.pop(4))
 # Remove the first IT company from the list
-
+print(companies.pop(0))
 # Remove the middle IT company or companies from the list
-
+print(companies.pop(2))
 # Remove the last IT company from the list
-
+print(companies.pop())
 # Remove all IT companies from the list
-
+print(companies.clear())
+print(companies)
 # Destroy the IT companies list
-
+del companies
+# print(companies)
 # Join the following lists:
 
-# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-# back_end = ['Node','Express', 'MongoDB']
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+fullstack = front_end+(back_end)
+print(fullstack)
 # After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
-
+full_stack=fullstack
+print(full_stack)
 # Exercises: Level 2
 # The following is a list of 10 students ages:
-# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 # Sort the list and find the min and max age
+ages.sort()
+print(ages)
+print(min(ages))
 # Add the min age and the max age again to the list
+# print(ages.append(min(ages) and max(ages)))
 # Find the median age (one middle item or two middle items divided by two)
 # Find the average age (sum of all items divided by their number )
 # Find the range of the ages (max minus min)
@@ -176,3 +190,4 @@ print(it_companies)
 # Find the middle country(ies) in the countries list
 # Divide the countries list into two equal lists if it is even if not one more country for the first half.
 # ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+# Slice out the first 3 companies from the list
---
+++ 05_Day_Lists/day_5/sorted.py
@@ -0,0 +1,5 @@
+def dictionaries(w):
+    x= (sorted(w.items()))
+    print(dict(x))
+
+dictionaries({'Bree':10,'Grace':30,'Asha':20})
