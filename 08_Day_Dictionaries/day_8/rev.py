26a4e1b0fe7215bf7b1384fce299dd9124a87d3b
CudraMohamed <ncudramohamed@gmail.com>
2022-06-07 22:31:27 +0300

    exc

--- 08_Day_Dictionaries/day_8/rev.py
+++ 08_Day_Dictionaries/day_8/rev.py
@@ -32,7 +32,38 @@ else:
     print(listOne == listTwo)
     print(listOne is listTwo)
 
-    for x in range(0.5,5.5,0.5):
-     print(x)       #float object can not be interpreted to integer
+    # for x in range(0.5,5.5,0.5):
+    #  print(x)       #float object can not be interpreted to integer
 
-     
+#Write a function for checking the speed of drivers. This function should have one parameter: speed.
+#If speed is less than 70, it should print “Ok”.
+#Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points. For example, if the speed is 80, it should print: “Points: 2”.
+#If the driver gets more than 12 points, the function should print: “License suspended”/
+
+def speed_check(speed):
+    speedlimit=70
+    demeritpoint=(speed-speedlimit*1)//5
+    if speed<70:
+        print('okay')
+    elif demeritpoint>12 & speed >70:
+        print('lisense suspend')
+    elif speed >70:
+        print(f'points:{demeritpoint}')
+    else:
+        print('none')
+speed_check(85)
+
+# What about we do this as the challenge for today?
+# Write a Python program to count the number of characters (character frequency) in a string.
+# Example: Sample String : google.com'
+# Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}#
+def count_char(word):
+    results={}
+    for x in word:
+        keys=results
+        if x in keys:
+            results[x]+=1
+        else:
+            results[x]=1
+    print(results)
+count_char('Cudra')
